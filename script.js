0b317631eee22e1fece7a22955e39d68164625ee
asal <asalrazavi2002@gmail.com>
2023-07-31 22:34:05 +0200

    add

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ let playgame=document.getElementById('game')
 let body=document.getElementById('body')
 let counter=document.querySelector('span')
 let count=0;
-let chackcounter=true;
+let checkcounter=true;
+
+
 playgame.addEventListener('click',function(){
   playgame.innerHTML='playgame'
   body.style.backgroundColor='gray';
@@ -13,26 +15,26 @@ let z=Math.floor(Math.random()*100)
 for(let i=0;i<sqr.length;i++){
     sqr[i].style. backgroundColor='rgb'+'('+ x +','+ y +','+ z +')';
 }
-let randomi=Math.floor(Math.random()*8)
+
 x=x-10;
-y=y-20
+y=y-20;
   let diferentsqr='rgb'+'('+ x +','+ y +','+ z +')';
+  let randomi=Math.floor(Math.random()*8)
+
+  console.log(randomi)
   sqr[randomi].style.backgroundColor=diferentsqr;
   let differentbtn= sqr[randomi]
-  chackcounter=true
-differentbtn.addEventListener('click',function(){
-    let win=document.createElement('p')
+  checkcounter=true;
+  differentbtn.addEventListener('click',function(){
+  
     playgame.innerHTML='winner'
     body.style.backgroundColor='green'
-    if(chackcounter){
+    if(checkcounter){
       count++;
       counter.innerHTML=count
-      chackcounter=false;
+      checkcounter=false;
     }
-  
-
+    console.log(randomi)
   })
-
-
 })
 
