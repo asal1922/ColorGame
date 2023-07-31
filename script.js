92ca589bfd2cbf417ce7d42871be71734bad696f
asal <asalrazavi2002@gmail.com>
2023-07-31 20:09:44 +0200

    add

---
+++ script.js
@@ -0,0 +1,37 @@
+let sqr=document.getElementsByClassName('sqr')
+let playgame=document.getElementById('game')
+let body=document.getElementById('body')
+let counter=document.querySelector('span')
+let count=0;
+let chackcounter=true;
+playgame.addEventListener('click',function(){
+  playgame.innerHTML='playgame'
+  body.style.backgroundColor='gray';
+let x=Math.floor(Math.random()*100)
+let y=Math.floor(Math.random()*100)
+let z=Math.floor(Math.random()*100)
+for(let i=0;i<sqr.length;i++){
+    sqr[i].style. backgroundColor='rgb'+'('+ x +','+ y +','+ z +')';
+}
+let randomi=Math.floor(Math.random()*8)
+x=x-10;
+y=y-20
+  let diferentsqr='rgb'+'('+ x +','+ y +','+ z +')';
+  sqr[randomi].style.backgroundColor=diferentsqr;
+  chackcounter=true
+  sqr[randomi].addEventListener('click',function(){
+    let win=document.createElement('p')
+    playgame.innerHTML='winner'
+    body.style.backgroundColor='green'
+    if(chackcounter){
+      count++;
+      counter.innerHTML=count
+      chackcounter=false;
+    }
+  
+
+  })
+
+
+})
+
